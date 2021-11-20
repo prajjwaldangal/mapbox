fbbbb7d403de0991a95b38333a7a4779dc436928
Prajjwal Dangal <pdangal@uccs.edu>
2021-11-19 17:36:46 -0700

    security patch

--- cs5420/airQualityVisualization/views.py
+++ cs5420/airQualityVisualization/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the air data visualization index.")	
+    template = loader.get_template('NewMainAQI.html')
+    return HttpResponse(template.render())
+    # return HttpResponse("Hello, world. You're at the air data visualization index.")	
--- cs5420/cs5420/settings.py
+++ cs5420/cs5420/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-and&y1+3^k@&!qtl(bv46@-j9-dw56$o371rhoi@d(q44%pdab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'airQualityVisualization',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cs5420.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['../airQualityVisualization'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
